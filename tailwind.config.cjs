d00bf0f20fa35171c85fec83ed9f78da455b30b9
cjsenteprise <corejsprog@gmail.com>
2023-10-07 08:36:16 -0600

    Page 0.25v Alpha

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+		colors: {
+			colors: {
+				nblue: '#001f3f',
+				rblue: '#0074e4',
+				lblue: '#4cb1e1',
+				iblue: '#a7dbd8',
+				pwhite: '#ffffff'
+			},
+		},
+	},
+	plugins: [],
+}
